7bbc57c48fb1cf4f2cb1e436a9d561f42495124f
Alejandro de la Rosa Zequeira <65790899+alejandroZequeira@users.noreply.github.com>
2023-01-24 17:05:35 -0600

    creacion de componente footer

---
+++ src/components/footer.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="block"><p>hola</p></div>
+    <div class="footer">
+        <img src="../assets/muyal-black.png">
+        <p> Contactanos a través de nuestros diferentes canales y conoce más sobre la Plataforma tecnológica para la gestión, aseguramiento, intercambio y preservación de grandes volúmenes de datos en salud y construcción de un repositorio nacional de servicios de análisis de datos de salud.</p>
+        
+        <div class="social-icons">
+            <a href="https://www.facebook.com/cinvestav.lti" target="_blank">
+                <i class="fa fa-facebook fa-lg"></i></a>
+            <a href="#">
+                <i class="fa fa-twitter fa-lg"></i></a>
+            <a href="#">
+                <i class="fa fa-instagram fa-lg"></i></a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+    .footer{
+        display: flex;
+        align-items: center;
+        align-content: center;
+        flex-direction: column;
+        padding: 20px;
+    }
+    .block{
+        width: 100%;
+        height: 40%;
+        background-color: var(--color-black);
+    }
+    .footer p{
+        text-align: center;
+    }
+    .footer img{
+        height: 10%;
+        width: 10%;
+    }
+</style>
--- src/views/Nez.vue
+++ src/views/Nez.vue
@@ -1,8 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is the Nez view</h1>
+
+  <div class="section-title">
+    <img src="..\assets\nez.png" alt="MUYAL_LOGO">
+    <h2>Servicio de construcción de sistemas e-salud</h2>
   </div>
+  <footerMuyual></footerMuyual>
 </template>
 
-<style>
+<script setup>
+  import footerMuyual from "../components/footer.vue"
+</script>
+
+<style >
+  .section-title{
+    display: flex;
+    background-color: #397DFF;
+    height: 100vh;
+    align-content: center;
+    flex-direction: column;
+    column-gap: 20px;
+  }
+
+  img{
+    align-self: center;
+    height: 50%;
+    width: 50%;
+  }
+  h2{
+    color: white;
+    text-align: center;
+    margin: 20px;
+  }
 </style>
