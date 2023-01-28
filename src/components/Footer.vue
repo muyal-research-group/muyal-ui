17960fc2fb336d40f876288d7468ee23c253deeb
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2023-01-28 17:00:38 -0600

    Footer component [fixed]

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,39 +2,47 @@
 
 </script>
 <template>
-    <div class="block"><p>hola</p></div>
-    <div class="footer">
-        <img src="../assets/muyal-black.png">
-        <p> Contactanos a través de nuestros diferentes canales y conoce más sobre la Plataforma tecnológica para la gestión, aseguramiento, intercambio y preservación de grandes volúmenes de datos en salud y construcción de un repositorio nacional de servicios de análisis de datos de salud.</p>
-        
-        <div class="social-icon">
-            <a href="https://www.facebook.com/cinvestav.lti" target="_blank">
-               <div class="circle"> 
-                    <img src="@/assets/facebook-icon-1.svg"> 
-                </div> 
-            </a>
-            <a href="#">
-                <div class="circle">
-                <img src="@/assets/instagram-glyph-1.svg"></div></a>
-            <a href="#">
-                <div class="circle"><img src="@/assets/twitter-6.svg">
-                </div></a>
+    <div class="footer-wrapper"> 
+        <div class="footer__header"></div>
+        <div class="footer">
+            <img src="../assets/muyal-black.png">
+            <p> Contactanos a través de nuestros diferentes canales y conoce más sobre la Plataforma tecnológica para la gestión, aseguramiento, intercambio y preservación de grandes volúmenes de datos en salud y construcción de un repositorio nacional de servicios de análisis de datos de salud.</p>
+            
+            <div class="footer__icons">
+                <a href="https://www.facebook.com/cinvestav.lti" target="_blank">
+                <div class="circle"> 
+                        <img src="@/assets/facebook-icon-1.svg"> 
+                    </div> 
+                </a>
+                <a href="#">
+                    <div class="circle">
+                    <img src="@/assets/instagram-glyph-1.svg"></div></a>
+                <a href="#">
+                    <div class="circle"><img src="@/assets/twitter-6.svg">
+                    </div></a>
+            </div>
+            <p>© Copyright 2020 - E-Salud by Cinvestav Tamaulipas</p>
         </div>
-        <p>© Copyright 2020 - E-Salud by Cinvestav Tamaulipas</p>
     </div>
 </template>
 
 <style>
+    .footer-wrapper{
+        position: relative;
+        width: 100vw;
+        display: block;
+    }
     .footer{
         display: flex;
         align-items: center;
         align-content: center;
         flex-direction: column;
         padding: 20px;
-    }
-    .block{
         width: 100%;
-        height: 40%;
+    }
+    .footer__header{
+        width: 100%;
+        height: 40px;
         background-color: black;
     }
     .footer p{
@@ -48,17 +56,17 @@
         height: 10%;
         width: 10%;
     }
-    .social-icon{
+    .footer__icons{
         align-self: center;
         display: flex;
     }
-    .social-icon img{
+    .footer__icons img{
         width: 30px;
         height: 30px;
         margin: 10px;
         filter: invert();
     }
-    .social-icon .circle{
+    .footer__icons .circle{
         background-color: black;
         margin: 10px;
     }
