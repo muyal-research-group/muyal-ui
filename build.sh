a12af1e51cbba5a68436593918e5d654534aee4a
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2024-04-07 14:22:22 -0600

    [updated/added] config files

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-rm -rf dist dist.tar || true && npm run build && tar -cvf dist.tar dist && scp ./dist.tar jcastillo@148.247.201.224:~/
+rm -rf dist dist.tar || true && npm run build && tar -cvf dist.tar dist && lz4 ./dist.tar && scp ./dist.tar.lz4 jcastillo@148.247.201.224:~/
